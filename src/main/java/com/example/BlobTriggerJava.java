194508faa0d53d6ab7105571890f1fb1f6f7ccd6
Harneet Kaur Chug <hchug@Harneets-MacBook-Pro.local>
2026-04-29 00:30:16 +0200

    initial commit

---
+++ src/main/java/com/example/BlobTriggerJava.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Azure Blob trigger.
+ */
+public class BlobTriggerJava {
+    /**
+     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
+     */
+    @FunctionName("BlobTriggerJava")
+    public void run(
+        @BlobTrigger(name = "content", path = "blobexamplehar/{name}", dataType = "binary",connection = "AzureWebJobsStorage") byte[] content,
+        @BindingName("name") String fileName,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Blob is triggered now......: " + fileName + "\n  Size: " + content.length + " Bytes");
+    }
+}
